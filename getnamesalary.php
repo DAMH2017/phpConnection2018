dfce26ba02f0cb855061e452ae454d2c0a86c0f5
Non person <nonemail@gmail.com>
2018-04-02 21:35:11 +0200

    add get name & salary

---
+++ getnamesalary.php
@@ -0,0 +1,15 @@
+<?php
+
+require 'php_connection.php';
+$getdata_query="select name,salary from employeetable";
+
+if($getdata_query)
+{
+while($row= mysqli_fetch_array($getdata_query))
+{
+  $flag[]=$row;  
+}
+print(json_encode($flag));
+}
+
+?>
