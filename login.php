7d706dbe91f9d532925a327ac2910c18ddfdf001
Non person <nonemail@gmail.com>
2018-04-02 17:16:10 +0200

    make php read inputs from android

--- login.php
+++ login.php
@@ -2,8 +2,8 @@
 
 require 'php_connection.php';
 
-$user_name="";
-$user_pass="";
+$user_name=$_POST["user_name"];
+$user_pass=$_POST["password"];
 $mysql_query="select * from employeetable where username like '$user_name' and password like '$user_pass';";
 $result= mysqli_query($conn, $mysql_query);
 
--- php_connection.php
+++ php_connection.php
@@ -5,12 +5,4 @@ $mysql_password="if9f2UKrZI";
 $server_name="sql12.freemysqlhosting.net";
 
 $conn=mysqli_connect($server_name,$mysql_username,$mysql_password,$db_name);
-if($conn)
-{
-    echo "Connection success";
-}
-else
-{
-    echo "Connection not success";
-}
 ?>
