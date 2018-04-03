cdbb5aa62c3e833d576fcf745cfb9ed01e49fcf2
Non person <nonemail@gmail.com>
2018-04-03 18:01:15 +0200

    make some corrections

--- getnamesalary.php
+++ getnamesalary.php
@@ -2,8 +2,8 @@
 
 require 'php_connection.php';
 $param1=$_POST["param1"];
-$param1=$_POST["param2"];
-$getdata_query="select name,salary from employeetable";
+$param2=$_POST["param2"];
+$getdata_query="select '$param1','$param2' from employeetable";
 $getDataResult= mysqli_query($conn, $getdata_query);
 
 if($getDataResult)
