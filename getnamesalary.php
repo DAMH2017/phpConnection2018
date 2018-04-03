add6961fa9d0eebe7289eccb8317e59e2ddb9bf4
Non person <nonemail@gmail.com>
2018-04-03 19:10:45 +0200

    from xAxis,yAxis to xData,yData

--- getnamesalary.php
+++ getnamesalary.php
@@ -3,7 +3,7 @@
 require 'php_connection.php';
 $xAxis=$_POST["xData"];
 $yAxis=$_POST["yData"];
-$getdata_query="select '.$xAxis.','.$yAxis.' from employeetable";
+$getdata_query="select ".$xAxis.",".$yAxis." from employeetable";
 $getDataResult= mysqli_query($conn, $getdata_query);
 
 if($getDataResult)
