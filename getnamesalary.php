6eaf105d1ec66b9c844003103b687228f522a403
Non person <nonemail@gmail.com>
2018-04-03 18:10:14 +0200

    change from param1, param2 to xAxis,yAxis

--- getnamesalary.php
+++ getnamesalary.php
@@ -1,8 +1,8 @@
 <?php
 
 require 'php_connection.php';
-$param1=$_POST["param1"];
-$param2=$_POST["param2"];
+$param1=$_POST["xAxis"];
+$param2=$_POST["yAxis"];
 $getdata_query="select '$param1','$param2' from employeetable";
 $getDataResult= mysqli_query($conn, $getdata_query);
 
