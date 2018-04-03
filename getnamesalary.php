253124e674409fab8d2ef2ed5a714af05790f20a
Non person <nonemail@gmail.com>
2018-04-03 18:36:32 +0200

    fix some errors

--- getnamesalary.php
+++ getnamesalary.php
@@ -3,7 +3,7 @@
 require 'php_connection.php';
 $xAxis=$_POST["xAxis"];
 $yAxis=$_POST["yAxis"];
-$getdata_query="select '$xAxis,'$yAxis from employeetable";
+$getdata_query="select '$xAxis','$yAxis' from employeetable";
 $getDataResult= mysqli_query($conn, $getdata_query);
 
 if($getDataResult)
